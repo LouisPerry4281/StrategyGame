2d1aa66eb182f01dfe10f0b072f0550e2bcf0459
LouisPerry4281 <louisperry4281@gmail.com>
2025-02-12 10:01:21 +0000

    SceneCreated

---
+++ StrategyGame/Assets/GridManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+     int _width, _height;
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+
+            }
+        }
+    }
+
+}
